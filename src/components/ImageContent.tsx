27cf817e9f822c3783be23416418c9e5653f0b79
Jac Rees <jac.rees@vodafone.com>
2025-02-08 14:44:35 +0000

    feat: ready

--- src/components/ImageContent.tsx
+++ src/components/ImageContent.tsx
@@ -3,7 +3,7 @@ function ImageContainer() {
     <div className="w-full h-[50vh] sm:h-[90vh] flex items-center justify-center relative">
       <div className="relative w-[60%] md:w-[80%] h-[60%] rounded-lg bg-[#313131] shadow-xl items-center justify-center hidden md:flex">
         <img
-          src="public/jacroyrees.jpeg"
+          src="jacroyrees.jpeg"
           alt="hello"
           className="absolute w-full h-full rounded-lg shadow-xl object-cover transform -translate-x-[5%] -translate-y-[5%]"
         />
